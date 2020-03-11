95e2d99ef3c3807c42301be19633f68eebb65e8e
Mtb-iago <iagooliveira09@outlook.com>
2020-03-11 00:14:12 -0300

    Projeto site front end 2th

---
+++ comentarios.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Comentarios</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/comentarios.css" rel="stylesheet">
+    </head>
+    <body>
+    <nav>
+    <!----direcionamento pelo url----->
+        <ul>
+            <li> <a href="index.php">Inicio</a></li>
+            <li> <a href="">Entrar</a></li>             
+        </ul>
+    </nav>
+    <div id="Largura-pagina" >
+    <section id="conteudo1">
+        <h1>vamos aqui colocar suas infos do seu site, do seu negocio, quem
+        sabe fechamos negocio lore impusin isso aei;</h1>
+        <img src="img\computador.jpg" >
+        <p class="texto">É um fato há muito estabelecido que um leitor se distrairá com o conteúdo legível de uma página ao analisar seu layout. O ponto de usar o Lorem Ipsum é que ele tem uma distribuição de letras mais ou menos normal, em vez de usar 'Conteúdo aqui, conteúdo aqui'.</p>
+        <p class="texto"> 1. O ponto de usar o Lorem Ipsum</p>
+        <p class="texto"> 2. È que ele tem uma distribuição de letras</p>
+        <p class="texto">3. Lorem Ipsum é que ele tem uma distribuição</p>
+        <p class="texto">4. letras mais ou menos normal</p>
+        </section>
+    </div>  
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Deixe seu comentario..</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/estilo.css" rel="stylesheet">
+    </head>
+    <body>
+    <nav>
+    <!----direcionamento pelo url----->
+        <ul>
+            <li> <a href="comentarios.php">Comentarios</a></li>
+            <li> <a href="">Entrar</a></li>             
+        </ul>
+    </nav>
+    <h3>CONTEUDO QUALQUER!!!!</h3>
+    </body>
+</html>
