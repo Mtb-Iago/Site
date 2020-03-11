498fce7d6890ec90f913bc205bfc2da975fd2c0e
Mtb-iago <iagooliveira09@outlook.com>
2020-03-11 18:03:47 -0300

    merge js

--- comentarios.php
+++ comentarios.php
@@ -14,17 +14,30 @@
             <li> <a href="">Entrar</a></li>             
         </ul>
     </nav>
-    <div id="Largura-pagina" >
+    <div id="Largura-pagina">
     <section id="conteudo1">
         <h1>vamos aqui colocar suas infos do seu site, do seu negocio, quem
         sabe fechamos negocio lore impusin isso aei;</h1>
-        <img src="img\computador.jpg" >
+        <img src="">
         <p class="texto">É um fato há muito estabelecido que um leitor se distrairá com o conteúdo legível de uma página ao analisar seu layout. O ponto de usar o Lorem Ipsum é que ele tem uma distribuição de letras mais ou menos normal, em vez de usar 'Conteúdo aqui, conteúdo aqui'.</p>
         <p class="texto"> 1. O ponto de usar o Lorem Ipsum</p>
         <p class="texto"> 2. È que ele tem uma distribuição de letras</p>
         <p class="texto">3. Lorem Ipsum é que ele tem uma distribuição</p>
         <p class="texto">4. letras mais ou menos normal</p>
+       <!-------sessao de comentarios do site--------->
         </section>
+        
+        <h2>Deixe seu comentário</h2>
+        <form method="POST">
+            <img src="img\perfil.png">
+            
+            <textarea name="Texto"  placeholder="Partice da discussão"></textarea> <!---placeholder texto que apaga quando se clica no botao---->
+            <input type="submit" value="PUBLICAR COMENTARIO"> <!--value e o nome que fica no botao-->
+            </form>
+            <input type="text"value="aaaaaaaaaaaa">
+        
+        
+
     </div>  
     </body>
 </html>
