4885e1b8ad646f6fdb1bece8cef57879f855214d
Mtb-iago <iagooliveira09@outlook.com>
2020-03-12 01:22:12 -0300

    css de comentário ok

--- comentarios.php
+++ comentarios.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <title>Comentarios</title>
+        <title>Deixe seu comentário</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="css/comentarios.css" rel="stylesheet">
@@ -18,23 +18,76 @@
     <section id="conteudo1">
         <h1>vamos aqui colocar suas infos do seu site, do seu negocio, quem
         sabe fechamos negocio lore impusin isso aei;</h1>
-        <img src="">
+        <img src="img\computador.jpg">
         <p class="texto">É um fato há muito estabelecido que um leitor se distrairá com o conteúdo legível de uma página ao analisar seu layout. O ponto de usar o Lorem Ipsum é que ele tem uma distribuição de letras mais ou menos normal, em vez de usar 'Conteúdo aqui, conteúdo aqui'.</p>
         <p class="texto"> 1. O ponto de usar o Lorem Ipsum</p>
         <p class="texto"> 2. È que ele tem uma distribuição de letras</p>
         <p class="texto">3. Lorem Ipsum é que ele tem uma distribuição</p>
         <p class="texto">4. letras mais ou menos normal</p>
-       <!-------sessao de comentarios do site--------->
-        </section>
-        
+        <!-------sessao de comentarios do site--------->
         <h2>Deixe seu comentário</h2>
         <form method="POST">
+        
+        <img src="img\perfil.png">
+        <textarea name="Texto"  placeholder="Participe da discussão"></textarea> <!---placeholder texto que apaga quando se clica no botao---->
+        <input type="submit" value="PUBLICAR COMENTARIO"> <!--value e o nome que fica no botao-->
+        </form>
+       
+        <div id="area-comentario">
             <img src="img\perfil.png">
+            <h3>nome da pessoa</h3>
+            <h4>horario e data <a href="">Excluir</a> </h4>
+            <p>Comentario da pessoa</p>
             
-            <textarea name="Texto"  placeholder="Partice da discussão"></textarea> <!---placeholder texto que apaga quando se clica no botao---->
-            <input type="submit" value="PUBLICAR COMENTARIO"> <!--value e o nome que fica no botao-->
-            </form>
-            <input type="text"value="aaaaaaaaaaaa">
+        </div>
+        <div id="area-comentario">
+            <img src="img\perfil.png">
+            <h3>nome da pessoa</h3>
+            <h4>horario e data <a href="">Excluir</a> </h4>
+            <p>Comentario da pessoa</p>
+            
+        </div>
+        <div id="area-comentario">
+            <img src="img\perfil.png">
+            <h3>nome da pessoa</h3>
+            <h4>horario e data <a href="">Excluir</a> </h4>
+            <p>Comentario da pessoa</p>
+            
+        </div>
+        <div id="area-comentario">
+            <img src="img\perfil.png">
+            <h3>nome da pessoa</h3>
+            <h4>horario e data <a href="">Excluir</a> </h4>
+            <p>Comentario da pessoa</p>
+            
+        </div>
+        </section>
+        <section id="conteudo2">
+            <div>
+                <img src="img\lateral.jpeg" >
+                <p>Analisar seu layout. O ponto de usar o Lorem Ipsum é que ele tem uma distribuição de letras
+                     mais ou menos normal, em vez de usar 'Conteúdo aqui, conteúdo aqui'</p>
+                
+            </div>
+            
+        </section>
+        <section id="conteudo3">
+            <div>
+                <h5>Saiba mais sobre como fazer</h5>
+                <p>Analisar seu layout. O ponto de usar o Lorem Ipsum é que ele tem uma distribuição de letras mais ou menos normal, em vez de usar 'Conteúdo aqui, conteúdo aqui'.</p>
+                
+            </div>
+            
+        </section>
+        
+       
+
+
+        
+        
+        
+        
+       
         
         
 
